6d0cdb3dade0f291709fec1dd93ebdf81276f66f
Nasrullo <nasrulloorinboyev1997@gmail.com>
2022-11-23 17:03:49 +0500

    chart

--- frontend/modules/manager/views/default/index.php
+++ frontend/modules/manager/views/default/index.php
@@ -1,12 +1,164 @@
-<div class="manager-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
-</div>
+
+
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+
+    <head>
+        
+        <meta charset="utf-8" />
+        <title>Dashboard | Upzet - Admin & Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesdesign" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        
+        <!-- jvectormap -->
+        <link href="assets/libs/jqvmap/jqvmap.min.css" rel="stylesheet" />
+
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" /> 
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body data-sidebar="dark">
+    
+    
+
+                        
+                        <div class="row">
+                            <div class="col-xl-3 col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="d-flex text-muted">
+                                        <div class="flex-shrink-0  me-3 align-self-center">
+                                                <div class="avatar-sm">
+                                                    <div class="avatar-title bg-light rounded-circle text-primary font-size-20">
+                                                        <i class="ri-group-line"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="flex-shrink-0 me-3 align-self-center">
+                                                <div id="radialchart-1" class="apex-charts" dir="ltr"></div>
+                                            </div>
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <h5 class="mb-3">2.2k</h5>
+                                                <p class="text-truncate mb-0"> Yangi ro'yxatha olinganlar</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- end card-body -->
+                                </div>
+                                <!-- end card -->
+                            </div>
+                            <!-- end col -->
+
+                            <div class="col-xl-3 col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="d-flex">
+                                        <div class="flex-shrink-0  me-3 align-self-center">
+                                                <div class="avatar-sm">
+                                                    <div class="avatar-title bg-light rounded-circle text-primary font-size-20">
+                                                        <i class="ri-group-line"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="flex-shrink-0 me-3 align-self-center">
+                                                <div id="radialchart-2" class="apex-charts" dir="ltr"></div>
+                                            </div>
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <h5 class="mb-3">50</h5>
+                                                <p class="text-truncate mb-0">Yangi guruhlar</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- end card-body -->
+                                </div>
+                                <!-- end card -->
+                            </div>
+                            <!-- end col -->
+
+                            <div class="col-xl-3 col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="d-flex text-muted">
+                                        <div class="flex-shrink-0  me-3 align-self-center">
+                                                <div class="avatar-sm">
+                                                    <div class="avatar-title bg-light rounded-circle text-primary font-size-20">
+                                                        <i class="ri-group-line"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="flex-shrink-0 me-3 align-self-center">
+                                                <div id="radialchart-3" class="apex-charts" dir="ltr"></div>
+                                            </div>
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <h5 class="mb-3">24.03 %</h5>
+                                                <p class="text-truncate mb-0"> Tasdiqlanganmagan to'lov</p>
+                                            </div>
+                                        </div>                                        
+                                    </div>
+                                    <!-- end card-body -->
+                                </div>
+                                <!-- end card -->
+                            </div>
+                            <!-- end col -->
+
+                            <div class="col-xl-3 col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="d-flex text-muted">
+                                            <div class="flex-shrink-0  me-3 align-self-center">
+                                                <div class="avatar-sm">
+                                                    <div class="avatar-title bg-light rounded-circle text-primary font-size-20">
+                                                        <i class="ri-group-line"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <h5 class="mb-3">435</h5>
+                                                <p class="text-truncate mb-0"> Jami to'lovlar (Noyabr</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- end card-body -->
+                                </div>
+                                <!-- end card -->
+                            </div>
+                            <!-- end col -->
+                        </div>
+                        
+
+        <div class="rightbar-overlay"></div>
+
+        <!-- JAVASCRIPT -->
+        <script src="assets/libs/jquery/jquery.min.js"></script>
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
+
+        <!-- apexcharts js -->
+        <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+
+        <!-- jquery.vectormap map -->
+        <script src="assets/libs/jqvmap/jquery.vmap.min.js"></script>
+        <script src="assets/libs/jqvmap/maps/jquery.vmap.usa.js"></script>
+
+        <script src="assets/js/pages/dashboard.init.js"></script>
+
+        <script src="assets/js/app.js"></script>
+
+    </body>
+</html>
